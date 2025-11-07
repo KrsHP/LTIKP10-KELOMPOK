586f26c154345dc6ac90f22b8e71c47843882837
KrsHP <kresna.hono.putro@student.upi.edu>
2025-11-07 18:14:45 +0700

    Adding auto printer for List Nama and making it a Procedure

--- 25_09_2025_SK3.py
+++ 25_09_2025_SK3.py
@@ -3,23 +3,39 @@
 # NIM: 2504879
 # Kelas: RPL 1B
 
+# Nama: Kresna Hono Putro
+# NIM: 2501870
+# Kelas: RPL 1B
+
 student_info = {
-    "Alice": {"age":20, "major": "Computer Science"},
-    "Bob": {"age": 21, "major": "Mathematics"},
-    "Charlie:": {"age": 19, "major": "Physics"},
+    "Alice": {"Umur":20, "Jurusan": "Rekayasa Perangkat Lunak"},
+    "Bob": {"Umur": 21, "Jurusan": "Matematika"},
+    "Charlie": {"Umur": 19, "Jurusan": "Fisika"},
+    "Kirk": {"Umur": 19, "Jurusan": "Biologi"},
 }
 
-print("""
-      +++++++++++++[List Nama]++++++++++++++
-      1.Alice
-      2.Bob
-      3.Charlie
-      +++++++++++++++++++++++++++++++++++++
-      """)
-user_input1= input("Masukan Nama Mahasiswa:")
+#Fungsi print ListNama
+def ListNama():
+    print("+++++++++++++[List Nama]++++++++++++++")
+    for i, nama_mahasiswa in enumerate(student_info.keys(), start = 1):
+        print(f"{i}. {nama_mahasiswa}")
+    print("++++++++++++++++++++++++++++++++++++++")
 
-if user_input1 in student_info:
-    info = student_info[user_input1]
-    print(f"Umur {user_input1} adalah {info['age']} dan prodinya adalah {info['major']}")
-else:
-    print("Tidak Valid")
+    user_input1= input("Masukan Nama Mahasiswa:")
+
+    if user_input1 in student_info:
+        info = student_info[user_input1]
+        print(f"Umur {user_input1} adalah {info['Umur']} dan prodinya adalah {info['Jurusan']}")
+    else:
+        print("Tidak Valid")
+
+def FungsiUI():
+    print("+++++++++++++[Selamat Datang]++++++++++++++")
+    print("1.Menambahkan Nama\n2.Melihat Nama\n3.Mengubah Nama\n4.Keluar")
+    print("+++++++++++++++++++++++++++++++++++++++++++\n")
+
+#Jalankan FungsiUI
+FungsiUI()
+
+#Jalankan fungsi ListNama
+ListNama()
